4692dc53fac300f9c6352ac5550ff521be26f531
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2017-08-16 17:34:39 +0100

    Implement cumulative normalised profiles

--- src/plot_3d.py
+++ src/plot_3d.py
@@ -10,11 +10,11 @@ hostId, snap = int(sys.argv[1]), int(sys.argv[2])
 reader = HBTReader("./data/")
 hosthalo = reader.LoadHostHalos(snap, selection=hostId)
 
-fig = plt.figure(figsize=plt.figaspect(2.0))
+fig = plt.figure(figsize=plt.figaspect(0.5))
 fig.suptitle("FoF group %d at snapshot %d"%(hostId, snap))
 
 # left panel - 3D partile positions
-ax = fig.add_subplot(2, 1, 1, projection='3d')
+ax = fig.add_subplot(1, 2, 1, projection='3d')
 
 ax.set_xlim3d([-5,5])
 ax.set_ylim3d([-5,5])
@@ -28,18 +28,20 @@ for trackId in reader.GetSubsOfHost(hostId, snap)['TrackId']:
 	ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
 
 # right panel - density profile
-ax = fig.add_subplot(2, 1, 2)
+ax = fig.add_subplot(1, 2, 2)
 
 ax.set_xlabel("r")
 ax.set_ylabel("differential mass")
 ax.set_xlim([-2.75, 0.25])
-ax.set_ylim([-3.0, 0.0])
+ax.set_ylim([-4.0, 0.0])
 
 prof, edges = reader.GetHostProfile(hostId, snap, bins=np.logspace(-2.5, 0.0, 32))
+prof = np.array(prof, dtype=np.float32)
+prof = np.cumsum(prof)
+prof = np.log10(np.divide(prof, prof[-1]))
 midpoints = 0.5*(np.log10(edges)[1:] + np.log10(edges)[:-1])
-prof = prof/float(prof[-1])
 
-ax.plot(midpoints, np.log10(prof))
+ax.plot(midpoints, prof)
 
 plt.tight_layout()
 fig.savefig("./plots/profile_%d/%03d.png"%(hostId,snap))
