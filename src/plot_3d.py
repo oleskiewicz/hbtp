1f6b86e36a7c96624fca49e7258965f2bea20097
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2017-08-16 14:24:07 +0100

    Adapt 3D plotter
    This version prodces CMH FoF merger animation in absolute reference
    frame.

--- src/plot_3d.py
+++ src/plot_3d.py
@@ -6,24 +6,27 @@ import matplotlib.pyplot as plt
 
 from HBTReader import HBTReader
 
-trackId, snap = int(sys.argv[1]), int(sys.argv[2])
+hostId, snap = int(sys.argv[1]), int(sys.argv[2])
 reader=HBTReader("./data/")
-particles = reader.GetProfile(trackId, snap)
+# subhalo = reader.GetSub(trackId, snap)
 
 fig = plt.figure(1)
 ax = Axes3D(fig)
-fig.suptitle("subhalo %d at snapshot %d"%(trackId, snap))
+fig.suptitle("FoF group %d at snapshot %d"%(hostId, snap))
 
-ax.set_xlim3d([-2,2])
-ax.set_ylim3d([-2,2])
-ax.set_zlim3d([-2,2])
+# ax.set_xlim3d([-2,2])
+# ax.set_ylim3d([-2,2])
+# ax.set_zlim3d([-2,2])
 
-ax.view_init(30, snap)
-ax.plot(particles[:, 0], particles[:, 1], particles[:, 2], 'b.')
-ax.plot([0.0], [0.0], [0.0], 'ro')
+ax.view_init(30, 2*snap)
 
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-ax.plot_wireframe(np.cos(u)*np.sin(v), np.sin(u)*np.sin(v), np.cos(v), color="r")
+for trackId in reader.GetSubsOfHost(hostId, snap)['TrackId']:
+	positions = reader.GetParticleProperties(trackId, snap)['ComovingPosition']
+	ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
+	# ax.plot([0.0], [0.0], [0.0], 'ro')
 
-fig.savefig("./plots/profile_%d/%03d.png"%(trackId,snap))
+	# u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+	# ax.plot_wireframe(np.cos(u)*np.sin(v), np.sin(u)*np.sin(v), np.cos(v), color="r")
+
+fig.savefig("./plots/profile_%d/%03d.png"%(hostId,snap))
 # plt.show()
