b86aff157dd9a3499db8685e8ebc769229e2322d
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2017-08-16 17:10:35 +0100

    Adapt 3D plotter
    Plot density profiles and 3D position simultaneously.

--- src/plot_3d.py
+++ src/plot_3d.py
@@ -10,9 +10,11 @@ hostId, snap = int(sys.argv[1]), int(sys.argv[2])
 reader = HBTReader("./data/")
 hosthalo = reader.LoadHostHalos(snap, selection=hostId)
 
-fig = plt.figure(1)
-ax = Axes3D(fig)
+fig = plt.figure(figsize=plt.figaspect(2.0))
 fig.suptitle("FoF group %d at snapshot %d"%(hostId, snap))
+
+# left panel - 3D partile positions
+ax = fig.add_subplot(2, 1, 1, projection='3d')
 
 ax.set_xlim3d([-5,5])
 ax.set_ylim3d([-5,5])
@@ -25,5 +27,20 @@ for trackId in reader.GetSubsOfHost(hostId, snap)['TrackId']:
 		- hosthalo['CenterComoving']) / hosthalo['R200CritComoving']
 	ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
 
+# right panel - density profile
+ax = fig.add_subplot(2, 1, 2)
+
+ax.set_xlabel("r")
+ax.set_ylabel("differential mass")
+ax.set_xlim([-2.75, 0.25])
+ax.set_ylim([-3.0, 0.0])
+
+prof, edges = reader.GetHostProfile(hostId, snap, bins=np.logspace(-2.5, 0.0, 32))
+midpoints = 0.5*(np.log10(edges)[1:] + np.log10(edges)[:-1])
+prof = prof/float(prof[-1])
+
+ax.plot(midpoints, np.log10(prof))
+
+plt.tight_layout()
 fig.savefig("./plots/profile_%d/%03d.png"%(hostId,snap))
 # plt.show()
