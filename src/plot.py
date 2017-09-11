3d134b006c5c2bcb7e32ad04cb7e0bad4c69e1bb
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2017-09-11 19:08:08 +0100

    Implement binning for plots

--- src/plot.py
+++ src/plot.py
@@ -3,6 +3,11 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+import logging
+from logging.config import fileConfig
+fileConfig("./log.conf")
+log = logging.getLogger()
+
 from src import read
 from src import process
 
@@ -15,19 +20,38 @@ def rho_c(z = 0.0):
   return Rho0*(OmegaM*np.power(1.0 + z, 3.)+OmegaL)
 
 def prof(ax, d):
-	x = np.logspace(-2.5, 0.0, 32)
-	d = process.normalise(d)
+	ax.set_title("FoF group density profile")
+	ax.set_xlabel(r"$\log_{10}(R/R_{200})$")
+	ax.set_ylabel(r"$\log_{10}(N(\mathrm{in shell}) / N(< R_{200}))$")
 	
+	x = np.linspace(-2.5, 0.0, 32)
+	d = np.array(d, dtype=np.float32)
+
+	# # cumulative
+	# d = process.normalise(d)
+
+	# # remove funny bins
+	# x = x[:-2]
+	# d = d[:,:-2]
+
+	# differential
+	d = np.divide(d.T, np.cumsum(d, axis=1)[:,-1]).T
+
 	for row in d:
 		ax.plot(x, np.log10(row), color='grey')
 	
 	ax.plot(x, np.log10(process.stack(d)),\
 		color='red', linewidth=2)
 
-def cmh(ax, d):
-	z =  read.snaps()
+def cmh(ax, d, isnap=122):
+	ax.set_title("FoF group collapsed mass history")
+	ax.set_xlabel(r"$\log_{10}(\rho_c(z) / \rho_c(z_0))$")
+	ax.set_ylabel(r"$\log_{10}(M_{200}\mathrm{progenitors})$")
+	
+	z = read.snaps()
 	x = rho_c(np.array([z[z['Snapshot'] == s][0]['Redshift']\
 		for s in map(lambda s: int(s), d.columns)]))
+	x = x/rho_c(z[z['Snapshot'] == isnap][0]['Redshift'])
 
 	d = process.normalise(d)
 	
@@ -38,7 +62,22 @@ def cmh(ax, d):
 
 if __name__ == '__main__':
 	snap = int(sys.argv[1])
-	fig, ax = plt.subplots(1)
-	d = read.cmh(snap).head(20)
-	cmh(ax, d)
-	plt.savefig("test.pdf")
+	z = read.snaps()
+	z0 = z[z['Snapshot'] == snap][0]['Redshift']
+
+	fig, ax = plt.subplots(figsize=(13,6), nrows=1, ncols=2)
+	fig.suptitle("Snapshot %d, $z_0=%.2f$"%(snap,z0))
+
+	# bin haloes by mass
+	idx = list(read.prop(snap)[:999]['mass_bin'] == 9)
+
+	data_cmh = read.cmh(snap).head(999)[idx]
+	cmh(ax[0], data_cmh, snap)
+
+	data_prof = read.prof(snap).head(999)[idx]
+	prof(ax[1], data_prof)
+
+	fig.tight_layout()
+	fig.subplots_adjust(top = 0.9)
+	plt.show()
+	# plt.savefig("%03d.pdf"%snap)
--- src/read.py
+++ src/read.py
@@ -3,24 +3,29 @@ import sys
 import numpy as np
 import pandas as pd
 
+from src import process
+
 def snaps():
 	d = np.genfromtxt("./output/hbtp/redshift_list.txt", delimiter=" ",\
 		dtype=np.dtype([('Snapshot', np.int32), ('Redshift', np.float32)]))
 	return d
 
 def prop(snap):
-  d = pd.read_csv("./output/hbtp/prop_%03d.csv"%snap)
-  # param['rhos'] = (param['rhos']*h*h*ParticleMass)/Rho0/rho_crit(z0) #TODO: normalisation
-  return d
+	d = pd.read_csv("./output/hbtp/prop_%03d.csv"%snap)
+	# param['rhos'] = (param['rhos']*h*h*ParticleMass)/Rho0/rho_crit(z0) #TODO: normalisation
+	d['mass_bin'] = process.bin(d, 'M200Crit',\
+		bins=np.linspace(9.0, 14.5, 11),\
+		transform=lambda m: np.log10(m*1.52e11))
+	return d
 
 def prof(snap):
-  d = pd.read_csv("./output/hbtp/prof_%03d.csv"%snap)
-  d.set_index('HostHaloId', inplace = True)
-  d.fillna(0.0, inplace = True)
-  return d
+	d = pd.read_csv("./output/hbtp/prof_%03d.csv"%snap)
+	d.set_index('HostHaloId', inplace = True)
+	d.fillna(0.0, inplace = True)
+	return d
 
 def cmh(snap):
-  d = pd.read_csv("./output/hbtp/cmh_%03d.csv"%snap)
-  d.set_index(['HostHaloId', 'IdentificationSnapshot'], inplace = True)
-  d.fillna(0.0, inplace = True)
-  return d
+	d = pd.read_csv("./output/hbtp/cmh_%03d.csv"%snap)
+	d.set_index(['HostHaloId', 'IdentificationSnapshot'], inplace = True)
+	d.fillna(0.0, inplace = True)
+	return d
