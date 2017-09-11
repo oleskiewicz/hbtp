76317ad2180b08f87964751e24d90d9065d8572b
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2017-09-11 19:09:53 +0100

    Remove profile from 3D plot

--- src/hbtp/plot_3d.py
+++ src/hbtp/plot_3d.py
@@ -4,19 +4,20 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-from HBTReader import HBTReader
+from src.hbtp.HBTReader import HBTReader
+from src import process
 
 if __name__ == '__main__':
-	host = 1
 	snap = int(sys.argv[1])
+	host = int(sys.argv[2])
 	reader = HBTReader("./data/")
 	hosthalo = reader.LoadHostHalos(snap, selection=host)
 
-	fig = plt.figure(figsize=plt.figaspect(0.5))
+	fig = plt.figure()#figsize=plt.figaspect(0.5))
 	fig.suptitle("FoF group %d at snapshot %d"%(host, snap))
 
 	# left panel - 3D partile positions
-	ax = fig.add_subplot(1, 2, 1, projection='3d')
+	ax = fig.add_subplot(1, 1, 1, projection='3d')
 
 	ax.set_xlim3d([-5,5])
 	ax.set_ylim3d([-5,5])
@@ -29,21 +30,23 @@ if __name__ == '__main__':
 			- hosthalo['CenterComoving']) / hosthalo['R200CritComoving']
 		ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
 
-	# right panel - density profile
-	ax = fig.add_subplot(1, 2, 2)
+	# # right panel - density profile
+	# ax = fig.add_subplot(1, 2, 2)
 
-	ax.set_xlabel("r")
-	ax.set_ylabel("differential mass")
-	ax.set_xlim([-2.75, 0.25])
-	ax.set_ylim([-4.1, 0.1])
+	# ax.set_xlabel("r")
+	# ax.set_ylabel("differential mass")
+	# ax.set_xlim([-2.75, 0.25])
+	# ax.set_ylim([-4.1, 0.1])
 
-	prof, edges = reader.GetHostProfile(host, snap, bins=np.logspace(-2.5, 0.0, 32))
-	prof = np.array(prof, dtype=np.float32)
-	prof = np.cumsum(prof)
-	prof = np.log10(np.divide(prof, prof[-1]))
-	# midpoints = 0.5*(np.log10(edges)[1:] + np.log10(edges)[:-1])
+	# prof, edges = reader.GetHostProfile(host, snap, bins=np.logspace(-2.5, 0.0, 33))
+	# prof = np.array(prof, dtype=np.float32)
+	# prof = np.cumsum(prof)
+	# prof = np.log10(np.divide(prof, prof[-1]))
+	# prof = process.normalise(prof)
 
-	ax.plot(edges[1:], prof)
+	# ax.plot(edges[1:], prof)
 
-	plt.tight_layout()
-	fig.savefig("./plots/profile_%d_%03d.png"%(host,snap))
+	# plt.tight_layout()
+
+	# fig.savefig("./plots/profile_%d_%03d.pdf"%(host,snap))
+	plt.show()
