2b05d1a07d8df6acefec0eefb687152904c2fcf2
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2019-01-28 13:46:51 +0000

    improve mass cut criterion

--- src/query.py
+++ src/query.py
@@ -8,28 +8,28 @@ import numpy as np
 from hbtp import HBTReader
 
 
-def main(grav, snap, min=1000):
+def main(grav, snap, min_part_num=1000):
     """Query & filter halo IDs.
 
     :param str grav: Gravity (GR_b64n512 or fr6_b64n512)
     :param int snap: Snapshot number (between 122 and 9)
-    :param int min: Minimum number of particles
+    :param int min_part_num: Minimum number of particles
     """
     logging.info("Loading snapshot %d of run %s" % (snap, grav))
 
     reader = HBTReader("./data/%s/subcat" % grav)
-    hosts = reader.LoadHostHalos(snap)
+    haloes = reader.LoadHostHalos(snap)
 
     # FILTER 1: halo mass cut
-    hosts = hosts[np.sum(hosts["Profile"], axis=1) >= min]
+    haloes = haloes[1e10 * haloes["M200Crit"] >= min_part_num * 1.52315180e8]
 
     # FILTER 2: CenterOffset
-    hosts = hosts[hosts["CenterOffset"] >= 0.07]
+    haloes = haloes[haloes["CenterOffset"] >= 0.07]
 
-    # # FILTER 3: orphan hosts - redundant
-    # hosts = [host for host in hosts if len(reader.GetSubsOfHost(host["HaloId"], snap)) > 0]
+    # # FILTER 3: orphan haloes - redundant
+    # haloes = [host for host in haloes if len(reader.GetSubsOfHost(host["HaloId"], snap)) > 0]
 
-    ids = hosts["HaloId"]
+    ids = haloes["HaloId"]
 
     logging.info("Found %d haloes" % (len(ids)))
 
