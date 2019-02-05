911837aef6e81bb6204b781a8228d1fde3115a1b
Piotr Oleskiewicz <piotr.oleskiewicz@durham.ac.uk>
2019-02-05 11:53:14 +0000

    update queue names

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -n 8
 #SBATCH -t 12:00:00
-#SBATCH -A dp004
-#SBATCH -p cosma6
+#SBATCH -A durham
+#SBATCH -p cosma
 #SBATCH -J hbtp_limin
 #SBATCH -o ./log/%A.out
 #SBATCH -e ./log/%A.err
--- submit.sh
+++ submit.sh
@@ -4,6 +4,6 @@ for NFW_f in 001 002 010; do
 	for G in GR_b64n512 fr6_b64n512; do
 		for S in 122 093 078 061 051; do
 			NFW_f=${NFW_f} GRAV=${G} SNAP=${S} sbatch ./run.sh
-		done;
-	done;
-done;
+		done
+	done
+done
